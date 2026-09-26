26a3b27fc11208da08ea61ca262008388983a13d
Bhavesh <bhavesh@kbe.com.kw>
2026-09-26 11:24:07 +0300

    reset password again

--- app/Http/Controllers/CustomerAuthController.php
+++ app/Http/Controllers/CustomerAuthController.php
@@ -8,6 +8,8 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Password;
+use Illuminate\Support\Str;
+use Illuminate\Auth\Events\PasswordReset;
 
 class CustomerAuthController extends Controller
 {
